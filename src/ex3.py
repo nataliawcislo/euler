eb246d8a53207585e66b2408dc06ae282e80e6b4
nataliawcislo <49069886+nataliawcislo@users.noreply.github.com>
2019-09-06 23:50:12 +0200

    Update ex3.py

--- src/ex3.py
+++ src/ex3.py
@@ -1,4 +1,9 @@
 def ex3(x):
+    '''
+    function which break down the number into prime numbers, and return max element with list
+    :param x: the number we break down into prime numbers
+    :return: max prime numbers
+    '''
     prime_factors = []
     i = 2
     while x != 1:
@@ -6,7 +11,7 @@ def ex3(x):
             prime_factors.append(i)
             x = x / i
         i += 1
-    return prime_factors
+    return max(prime_factors)
 
 
 print(ex3(600851475143))
